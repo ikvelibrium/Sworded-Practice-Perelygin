fdf5acecae8958188ad78ba400cff901a16525dc
ikvelibrium <stalker.alex.perel@mail.ru>
2023-06-27 17:57:55 +0300

    Worke started

--- Assets/Scripts/PlayerIndex.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerIndex : MonoBehaviour
-{
-    public int index;
-    public PlayerStats playerStats;
-}
